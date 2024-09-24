bd693af9e6e88ced842309ab6afdbd873c799dec
Ali-HkH <alikheirkhah.akk@gmail.com>
2024-09-24 21:23:08 +0330

    fixed section heading for colors

--- src/Components/Quote/Quote.js
+++ src/Components/Quote/Quote.js
@@ -5,21 +5,18 @@ function Quote() {
   return (
     <div className='relative bg-emerald-500 text-white z-10 pt-5 md:py-[50px] xl:pt-5 xl:pb-0'>
         {/* shape 1 */}
-        <div className='absolute w-[250px] bottom-0 right-0 opacity-50 -z-10 hidden lg:block'>
+        <div className='absolute bottom-0 right-0 w-[250px] opacity-50 -z-10 hidden lg:block'>
             <img src="./imgs/shape-1.png" alt="shape" />
         </div>
         {/* shape 2 */}
-        <div className='absolute top-5 left-10 hidden -z-10 lg:block'>
+        <div className='absolute top-5 left-10 -z-10 hidden lg:block'>
             <img src="./imgs/shape-2.png" alt="shape" />
         </div>
         <div className='container'>
             <div className='flex flex-col gap-y-6 items-start justify-center md:flex-row md:items-center  pt-5'>
                 <div className='px-3'>
-                    <div className="text-right mb-6">
-                        <span className="mb-[18px] inline-block text-sm lg:text-base lg:font-Dana-med">دریافت یک نقل قول</span>
-                        <h1 className="text-2xl lg:text-[40px]/[46px] font-Dana-bold">اگر شک دارید قیمت وام مسکن را دریافت کنید</h1>
-                    </div>
-                    <div className='flex gap-x-8 text-sm lg:text-base child:transition-colors'>
+                    <SectionHeading title="اگر شک دارید، قیمت وام مسکن را دریافت کنید" subTitle="دریافت یک نقل قول" center={false} white />
+                    <div className='flex gap-x-8 text-sm lg:text-base child:transition-colors mt-6'>
                         <a href="#" className='py-3 px-4 lg:py-4 lg:px-6 rounded-md bg-about hover:text-emerald-500 hover:bg-white'>به انجمن بپیوندید</a>
                         <a href="#" className='py-3 px-4 lg:py-4 lg:px-8 rounded-md bg-white text-emerald-500 hover:bg-about hover:text-white'>دریافت اطلاعات</a>
                     </div>
--- src/Components/SectionHeading/SectionHeading.js
+++ src/Components/SectionHeading/SectionHeading.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function SectionHeading({title, subTitle, center}) {
+function SectionHeading({title, subTitle, center, white}) {
   return (
     <div className={`${center? "text-center pb-[45px]" : "text-right"} `}>
-        <span className={`${center ? "mb-3" : "mb-[18px]"} inline-block text-sm lg:text-base lg:font-Dana-med text-emerald-500`}>{subTitle}</span>
+        <span className={`${center ? "mb-3" : "mb-[18px]"} ${white ? "text-white" : "text-emerald-500"} inline-block text-sm lg:text-base lg:font-Dana-med `}>{subTitle}</span>
         <h1 className={`${center? "text-3xl" : "text-2xl"} lg:text-[40px]/[46px] font-Dana-bold`}>{title}</h1>
     </div>
   )
