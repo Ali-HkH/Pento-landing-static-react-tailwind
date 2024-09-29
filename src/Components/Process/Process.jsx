32c9e36105bdac4b8f14ff086733150ab0006379
Ali-HkH <alikheirkhah.akk@gmail.com>
2024-09-29 23:58:29 +0330

    fixed accordion bug in process section

--- src/Components/Process/Process.jsx
+++ src/Components/Process/Process.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import SectionHeading from "../SectionHeading/SectionHeading";
 import ProcessItem from "../ProcessItem/ProcessItem";
 
 function Process() {
+   const [openedProcess, setOpenedProcess] = useState(1);
+
    return (
       <div id="process">
          <div className="container text-zinc-800 py-[50px] md:py-[70px] lg:py-[100px]">
@@ -13,11 +15,36 @@ function Process() {
             />
             <div className="flex flex-col items-start gap-y-5 lg:flex-row lg:items-center lg:gap-x-5">
                <div className="px-3">
-                  <ProcessItem text="ارزیابی" number="1" />
-                  <ProcessItem text="گفتوگو" number="2" />
-                  <ProcessItem text="کاربرد" number="3" />
-                  <ProcessItem text="اسناد" number="4" />
-                  <ProcessItem text="پذیره نویسی اولیه" number="5" />
+                  <ProcessItem
+                     text="ارزیابی"
+                     number="1"
+                     openedProcess={openedProcess}
+                     setOpenedProcess={setOpenedProcess}
+                  />
+                  <ProcessItem
+                     text="گفتوگو"
+                     number="2"
+                     openedProcess={openedProcess}
+                     setOpenedProcess={setOpenedProcess}
+                  />
+                  <ProcessItem
+                     text="کاربرد"
+                     number="3"
+                     openedProcess={openedProcess}
+                     setOpenedProcess={setOpenedProcess}
+                  />
+                  <ProcessItem
+                     text="اسناد"
+                     number="4"
+                     openedProcess={openedProcess}
+                     setOpenedProcess={setOpenedProcess}
+                  />
+                  <ProcessItem
+                     text="پذیره نویسی اولیه"
+                     number="5"
+                     openedProcess={openedProcess}
+                     setOpenedProcess={setOpenedProcess}
+                  />
                </div>
                <div className="px-3 w-full lg:w-auto">
                   <img src="./imgs/process-img.png" alt="process-img" />
--- src/Components/ProcessItem/ProcessItem.jsx
+++ src/Components/ProcessItem/ProcessItem.jsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
 
-function ProcessItem({ text, number }) {
+function ProcessItem({ text, number, openedProcess, setOpenedProcess }) {
    const [isProcessExpand, setIsProcessExpand] = useState(false);
+
+   const accordionClickHandler = (number) => {
+      if (number !== openedProcess) {
+         setOpenedProcess(number);
+         setIsProcessExpand(true);
+      } else {
+         setIsProcessExpand(!isProcessExpand);
+      }
+   };
 
    return (
       <div
@@ -11,7 +20,7 @@ function ProcessItem({ text, number }) {
       >
          <button
             className="pb-4 relative"
-            onClick={() => setIsProcessExpand(!isProcessExpand)}
+            onClick={() => accordionClickHandler(number)}
          >
             <span className="absolute -right-6 -top-3 size-[50px] bg-emerald-500 text-white rounded-full  flex-center">
                {number}
@@ -22,7 +31,7 @@ function ProcessItem({ text, number }) {
          </button>
          <div
             className={`${
-               isProcessExpand
+               number === openedProcess && isProcessExpand
                   ? "max-h-[120px] opacity-100 "
                   : "max-h-0 opacity-0 "
             }  lg:w-[376px] xl:w-[476px] 2xl:w-[576px] overflow-hidden pr-10 pb-5 text-[15px] font-Dana-med transition-all duration-300`}
